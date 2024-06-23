42c9822f833f2d2e335419bc46ea5b710016ef6f
jaime111111 <jaime6030@gmail.com>
2024-06-23 01:20:24 -0400

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,7 +1,3 @@
-// Importa la biblioteca de WebSocket
-const WebSocket = require('ws');
-
-// Configura el URL del WebSocket y los datos de autenticación
 const wsUrl = 'wss://us-pconnect7.coolkit.cc:443/api/ws';
 
 const authData = {
@@ -14,7 +10,7 @@ const authData = {
     userAgent: "app"
 };
 
-const controlData = {
+const controlDataOn = {
     action: "update",
     deviceid: "1001fdd44c",
     apikey: "8bbcab39-b708-49be-8366-98f1d47f9456",
@@ -25,57 +21,62 @@ const controlData = {
     sequence: Date.now().toString()
 };
 
-// Función principal que manejará el webhook
-module.exports = async (req, res) => {
-    // Loguea la recepción del webhook
-    console.log('Webhook recibido');
-    
-    // Crea una nueva instancia de WebSocket
-    const ws = new WebSocket(wsUrl);
+const controlDataOff = {
+    action: "update",
+    deviceid: "1001fdd44c",
+    apikey: "8bbcab39-b708-49be-8366-98f1d47f9456",
+    userAgent: "app",
+    params: {
+        switch: "off"
+    },
+    sequence: Date.now().toString()
+};
 
-    // Manejadores de eventos para el WebSocket
-    ws.on('open', () => {
-        console.log(`Conectado a WebSocket en ${wsUrl}`);
-        ws.send(JSON.stringify(authData));
-        console.log(`Mensaje de autenticación enviado: ${JSON.stringify(authData)}`);
-    });
+const ws = new WebSocket(wsUrl);
 
-    ws.on('message', (data) => {
-        console.log(`Mensaje recibido: ${data}`);
-        const message = JSON.parse(data);
-        if (message.error === 0 && message.config && message.config.hbInterval) {
-            setTimeout(() => {
-                const controlDataWithSequence = { ...controlData, sequence: Date.now().toString() };
-                ws.send(JSON.stringify(controlDataWithSequence));
-                console.log(`Enviando mensaje de control para encender: ${JSON.stringify(controlDataWithSequence)}`);
-            }, 2000); // Pausa de 2000 milisegundos (2 segundos) antes de enviar el comando
-            setInterval(() => {
-                ws.send('ping');
-                console.log('Heartbeat enviado');
-            }, message.config.hbInterval * 1000);
-        } else if (message.error !== 0) {
-            console.log(`Error recibido: ${message.error}`);
+ws.onopen = () => {
+    console.log(`Conectado a WebSocket en ${wsUrl}`);
+    ws.send(JSON.stringify(authData));
+    console.log(`Mensaje de autenticación enviado: ${JSON.stringify(authData)}`);
+};
+
+ws.onmessage = (event) => {
+    console.log(`Mensaje recibido: ${event.data}`);
+    const message = JSON.parse(event.data);
+    if (message.error === 0 && message.config && message.config.hbInterval) {
+        setTimeout(() => {
+            const controlDataOnWithSequence = { ...controlDataOn, sequence: Date.now().toString() };
+            ws.send(JSON.stringify(controlDataOnWithSequence));
+            console.log(`Enviando mensaje de control para encender: ${JSON.stringify(controlDataOnWithSequence)}`);
+        }, 2000); // Pausa de 2000 milisegundos (2 segundos) antes de enviar el comando de encendido
+
+        setTimeout(() => {
+            const controlDataOffWithSequence = { ...controlDataOff, sequence: Date.now().toString() };
+            ws.send(JSON.stringify(controlDataOffWithSequence));
+            console.log(`Enviando mensaje de control para apagar: ${JSON.stringify(controlDataOffWithSequence)}`);
+        }, 4000); // Pausa de 4000 milisegundos (4 segundos) antes de enviar el comando de apagado
+
+        setInterval(() => {
+            ws.send('ping');
+            console.log('Heartbeat enviado');
+        }, message.config.hbInterval * 1000);
+    } else if (message.error !== 0) {
+        console.log(`Error recibido: ${message.error}`);
+    }
+
+    if (message.action === 'update') {
+        if (message.error === 0) {
+            console.log(`Comando aceptado: ${JSON.stringify(message)}`);
+        } else {
+            console.log(`Error al ejecutar el comando: ${message.error}`);
         }
+    }
+};
 
-        if (message.action === 'update') {
-            if (message.error === 0) {
-                console.log(`Comando aceptado: ${JSON.stringify(message)}`);
-            } else {
-                console.log(`Error al ejecutar el comando: ${message.error}`);
-            }
-        }
-    });
+ws.onclose = () => {
+    console.log('Conexión WebSocket cerrada');
+};
 
-    ws.on('close', () => {
-        console.log('Conexión WebSocket cerrada');
-        res.status(200).json({ message: 'Conexión cerrada' });
-    });
-
-    ws.on('error', (error) => {
-        console.log(`Error en WebSocket: ${error.message}`);
-        res.status(500).json({ error: 'Error en WebSocket' });
-    });
-
-    // Enviar una respuesta inicial al webhook
-    res.status(200).json({ message: 'Webhook recibido' });
+ws.onerror = (error) => {
+    console.log(`Error en WebSocket: ${error.message}`);
 };
